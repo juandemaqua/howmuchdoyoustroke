99b9f1c314aab370c169ce40df1e36f715051ef3
dm769 <dm769@cornell.edu>
2024-05-01 23:09:20 -0400

    update

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 const HomePage = () => (
-    <center>
-        <h1>Welcome!</h1>
-    </center>
+    <div className="App">
+      <header className="header">
+        {/* <img src={logo} className="logo" alt="logo" /> */}
+        <h1> What The Strokes?</h1>
+      </header>
+      <body className='body'>
+        <h2> A superfan's game to prove they're a superfan.</h2>
+        <button className='body_buttons'>Create an account</button>
+        <button className='body_buttons'>Login</button>
+      </body>
+    </div>
 );
 
 export default HomePage;
